68cf0f83f0e35c81ce59f2587e0e19bd7c90c39a
philipyexushen <923822778@qq.com>
2018-10-09 10:11:04 +0800

    add DecisionTree.py

---
+++ DecisionTree.py
@@ -0,0 +1,52 @@
+from Common import *
+from DataManager import *
+from matplotlib import pyplot as plt
+
+class TreeNode:
+    def __init__(self):
+        self.__category = None
+        self.__child = []
+
+    @property
+    def category(self):
+        return self.__category
+
+    @category.setter
+    def category(self, value):
+        self.__category = value
+
+    @property
+    def child(self):
+        return self.__child
+
+
+class DecisionTreeBuilder:
+    def __init__(self):
+        mgr = ExcelDataManager()
+        self.__data_set = mgr.fetch()
+
+    @property
+    def data_set(self):
+        return self.__data_set
+
+    def __tree_generate(self, D, A, root:TreeNode):
+        classified_list = self.__get_classified_list(D)
+        # 看下数据集的元素是不是都是同一个类别的
+        if len(classified_list) == 1:
+            root.category = classified_list[0]
+        
+
+
+    @staticmethod
+    def __get_classified_list(D):
+        return [item[-1] for item in D]
+
+    def tree_generate(self):
+        classified_list = self.__get_classified_list(self.data_set)
+        root = TreeNode()
+        return self.__tree_generate(self.data_set, classified_list, root)
+
+
+if __name__ == "__main__":
+    builder = DecisionTreeBuilder()
+    builder.tree_generate()
--- FindBestDivisionPoint.py
+++ FindBestDivisionPoint.py
@@ -1,4 +1,3 @@
-import numpy as np
 from DataManager import *
 from Common import *
 
@@ -11,16 +10,18 @@ def get_position_and_negative_sum(data_set, left, right):
             sum_neg += 1
     return sum_pos, sum_neg
 
-if __name__ == "__main__":
+
+def find_gain_rate(column_index):
     mgr = ExcelDataManager()
     data_set = mgr.fetch()
-    data_set = sorted(data_set, key=lambda item: item[7])
+    data_set = sorted(data_set, key=lambda item: item[column_index])
     print(data_set)
 
-    Ta = [round((float(data_set[i - 1][7]) + float(data_set[i][7])) / 2, 3) for i in range(1, len(data_set))]
+    Ta = [round((float(data_set[i - 1][column_index]) + float(data_set[i][column_index])) / 2, 3) for i in range(1, len(data_set))]
 
     sum_pos, sum_neg = get_position_and_negative_sum(data_set, 0, len(data_set))
-    EntD = -sum_pos / len(data_set) * np.log2(sum_pos / len(data_set)) - sum_neg / len(data_set) * np.log2(sum_neg / len(data_set))
+    EntD = -sum_pos / len(data_set) * np.log2(sum_pos / len(data_set)) - sum_neg / len(data_set) * np.log2(
+        sum_neg / len(data_set))
 
     GainD_list = []
     for i, t in enumerate(Ta):
@@ -30,17 +31,25 @@ if __name__ == "__main__":
         sum_pos, sum_neg = get_position_and_negative_sum(data_set, 0, index)
 
         if sum_pos != 0 and sum_neg != 0:
-            GainD -= index / len(data_set)*(-sum_pos / index * np.log2(sum_pos / index) - sum_neg / index * np.log2(sum_neg / index))
+            GainD -= index / len(data_set) * (
+                        -sum_pos / index * np.log2(sum_pos / index) - sum_neg / index * np.log2(sum_neg / index))
 
         sum_pos, sum_neg = get_position_and_negative_sum(data_set, index, len(data_set))
 
         if sum_pos != 0 and sum_neg != 0:
-            GainD -= (len(data_set) - index)/ len(data_set)*(-sum_pos / (len(data_set) - index) * np.log2(sum_pos / (len(data_set) - index)) \
-                     - sum_neg / (len(data_set) - index) * np.log2(sum_neg / (len(data_set) - index)))
+            GainD -= (len(data_set) - index) / len(data_set) * (
+                        -sum_pos / (len(data_set) - index) * np.log2(sum_pos / (len(data_set) - index)) \
+                        - sum_neg / (len(data_set) - index) * np.log2(sum_neg / (len(data_set) - index)))
 
         GainD_list.append([GainD, t])
 
-    print(max(GainD_list, key=lambda x: x[0]))
+    result = max(GainD_list, key=lambda x: x[0])
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    find_gain_rate(8)
 
 
 
