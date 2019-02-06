7f161da1fb43cf5b72b7b90396d95738f6810017
philipyexushen <923822778@qq.com>
2019-02-07 02:54:19 +0800

    Add SMO Platt

--- SMOPlatt.py
+++ SMOPlatt.py
@@ -14,8 +14,17 @@ class SMOPlatt:
         self.__alphas = np.mat(np.zeros((m, 1)))
         self.__eCache = np.mat(np.zeros((m, 2)))
         self.__b = 0
-        
 
+        # 用核函数映射一下
+        self.__K = np.mat(np.zeros((m, m)))
+        for i in range(m):
+            self.__K[:, i] = kernel_function(self.__data_set, self.__data_set[i, :])
+
+    @property
+    def alphas(self): return self.__alphas
+
+    @property
+    def b(self): return self.__b
 
     @staticmethod
     def __clip_alpha(ai, H, L):
@@ -30,85 +39,129 @@ class SMOPlatt:
             j = int(random.uniform(0, data_range))
         return j
 
-    @property
-    def alphas(self):
-        return self.__alphas
+    def __unique_random_cache(self, index, Ei, alphas, label_mat, data_matrix, b):
+        maxK = -1; maxDeltaE = 0; Ej = 0
+        self.__eCache[index] = [1, Ej]  # set valid #choose the alpha that gives the maximum delta E
+        m, n = np.shape(self.__data_set)
 
-    @property
-    def b(self):
-        return self.__b
+        valid_cache_list = np.nonzero(self.__eCache[:, 0].A)[0]
+        if (len(valid_cache_list)) > 1:
+            for k in valid_cache_list:  # loop through valid Ecache values and find the one that maximizes delta E
+                if k == index: continue  # don't calc for i, waste of time
+                Ek = self.__get_Ek(index, alphas, label_mat, data_matrix, b)
 
-    def __get_Ek(self):
-        pass
+                deltaE = abs(Ei - Ek)
+                if deltaE > maxDeltaE:
+                    maxK = k
+                    maxDeltaE = deltaE
+                    Ej = Ek
+            return maxK, Ej
+
+        else:  # in this case (first time around) we don't have any valid eCache values
+            j = self.__unique_random(index, m)
+            Ek = self.__get_Ek(index, alphas, label_mat, data_matrix, b)
+
+        return j, Ek
+
+    @staticmethod
+    def __get_Ek(k, alphas, label_mat, data_matrix, b):
+        fxi = float(np.multiply(alphas, label_mat).T * (data_matrix * data_matrix[k, :].T)) + b
+        Ek = fxi - float(label_mat[k])
+        return Ek
+
+    def __updateEk(self, k, alphas, label_mat, data_matrix, b):
+        # after any alpha has changed update the new value in the cache
+        Ek = self.__get_Ek(k, alphas, label_mat, data_matrix, self.__b)
+        self.__eCache[k] = [1, Ek]
+
 
     def apply(self, C, toler = 1e-3, max_iter = 10000):
+        entireSet = True
+        alphaPairsChanged:bool = False
+        it = 0
+
+        m, n = np.shape(self.__data_set)
+        while (it < max_iter) and ((alphaPairsChanged == True) or entireSet):
+            alphaPairsChanged = False
+            if entireSet:  # go over all
+                for i in range(m):
+                    ret = self.__inner_apply(i, C, toler, max_iter)
+                    alphaPairsChanged = ret or alphaPairsChanged
+                it += 1
+            else:  # go over non-bound (railed) alphas
+                nonBoundIs = np.nonzero((self.alphas.A > 0) * (self.alphas.A < C))[0]
+                for i in nonBoundIs:
+                    ret = self.__inner_apply(i, C, toler, max_iter)
+                    alphaPairsChanged = ret or alphaPairsChanged
+                it += 1
+            if entireSet:
+                entireSet = False  # toggle entire set loop
+            elif not alphaPairsChanged:
+                entireSet = True
+
+
+    def __inner_apply(self, i, C, toler, max_iter):
         data_matrix = np.mat(self.__data_set)
         label_mat = np.mat(self.__label_mat).transpose()
-        alphas = self.__alphas
+        alphas = self.alphas
         m, n = np.shape(data_matrix)
-        b = 0
+        Ei = self.__get_Ek(i, alphas, label_mat, data_matrix, self.__b)
 
-        it = 0
-        while it < max_iter:
-            alphaPairsChanged: bool = False
-            for i in range(m):
-                # Ei = f(xi) - yi
-                # f(xi) = sum(w* x + b)
-                fxi = float(np.multiply(alphas, label_mat).T * (data_matrix * data_matrix[i, :].T)) + b
-                Ei = fxi - float(label_mat[i])
+        # Ei是误差，所以下面的意思是，误差越大，越值得优化，并且alpha已经保证了不能在边界上(0和C)了（看下面的代码的clip）
+        # magic代码，可以加快迭代速度，但是不知道为什么
+        if ((label_mat[i] * Ei < -toler) and (alphas[i] < C)) or ((label_mat[i] * Ei > toler) and (alphas[i] > 0)):
+            j = self.__unique_random(i, m)
+            Ej = self.__get_Ek(j, alphas, label_mat, data_matrix, self.__b)
 
-                # Ei是误差，所以下面的意思是，误差越大，越值得优化，并且alpha已经保证了不能在边界上(0和C)了（看下面的代码的clip）
-                # magic代码，可以加快迭代速度，但是不知道为什么
-                if ((label_mat[i] * Ei < -toler) and (alphas[i] < C)) or (
-                        (label_mat[i] * Ei > toler) and (alphas[i] > 0)):
-                    j = self.__unique_random(i, m)
-                    fxj = float(np.multiply(alphas, label_mat).T * (data_matrix * data_matrix[j, :].T)) + b
-                    Ej = fxj - float(label_mat[j])
-                    alpha_i_old = alphas[i].copy(); alpha_j_old = alphas[j].copy()
+            alpha_i_old = alphas[i].copy(); alpha_j_old = alphas[j].copy()
 
-                    # 方形约束
-                    if label_mat[i] != label_mat[j]:
-                        L = max(0, alphas[j] - alphas[i])
-                        H = min(C, C + alphas[j] - alphas[i])
-                    else:
-                        L = max(0, -C + alphas[j] + alphas[i])
-                        H = min(C, alphas[j] + alphas[i])
-                    if L == H:
-                        continue
-                    # eta = -(K11 + K22 - 2K12)
-                    eta = -data_matrix[i, :] * data_matrix[i, :].T - data_matrix[j, :] * data_matrix[j, :].T \
-                          + 2 * data_matrix[i, :] * data_matrix[j, :].T
+            # 方形约束
+            if label_mat[i] != label_mat[j]:
+                L = max(0, alphas[j] - alphas[i])
+                H = min(C, C + alphas[j] - alphas[i])
+            else:
+                L = max(0, -C + alphas[j] + alphas[i])
+                H = min(C, alphas[j] + alphas[i])
+            if L == H:
+                return False
 
-                    # a2new = a2Old + yj * (E1 - E2) / eta ==> update a2
-                    if eta == 0:
-                        continue
+            # eta = -(K11 + K22 - 2K12)
+            eta = -data_matrix[i, :] * data_matrix[i, :].T - data_matrix[j, :] * data_matrix[j, :].T \
+                  + 2 * data_matrix[i, :] * data_matrix[j, :].T
 
-                    alphas[j] -= label_mat[j] * (Ei - Ej) / eta
-                    alphas[j] = self.__clip_alpha(alphas[j], H, L)
+            # a2new = a2Old + yj * (E1 - E2) / eta ==> update a2
+            if eta == 0:
+                return False
 
-                    # 更新量太少了，直接忽略好了
-                    if abs(alphas[j] - alpha_j_old) < 1e-6:
-                        # print("j not moving enough")
-                        continue
+            alphas[j] -= label_mat[j] * (Ei - Ej) / eta
+            alphas[j] = self.__clip_alpha(alphas[j], H, L)
 
-                    # a1new = a1Old + yi* yj * (a2Old - a2new) / eta ==> update a1
-                    alphas[i] += label_mat[j] * label_mat[i] * (alpha_j_old - alphas[j])
+            # 缓存一次Ej
+            self.__updateEk(j, alphas, label_mat, data_matrix, self.__b)
 
-                    # 通过a更新b
-                    b1 = b - Ei - float(
-                        label_mat[i] * (alphas[i] - alpha_i_old) * data_matrix[i, :] * data_matrix[i, :].T) - \
-                         float(label_mat[j] * (alphas[j] - alpha_j_old) * data_matrix[i, :] * data_matrix[j, :].T)
-                    b2 = b - Ej - float(
-                        label_mat[i] * (alphas[i] - alpha_i_old) * data_matrix[i, :] * data_matrix[j, :].T) - \
-                         float(label_mat[j] * (alphas[j] - alpha_j_old) * data_matrix[j, :] * data_matrix[j, :].T)
+            # 更新量太少了，直接忽略好了
+            if abs(alphas[j] - alpha_j_old) < 1e-6:
+                # print("j not moving enough")
+                return False
 
-                    if 0 < alphas[i] < C:
-                        b = b1
-                    elif 0 < alphas[j] < C:
-                        b = b2
-                    else:
-                        b = (b1 + b2) / 2.0
-                    alphaPairsChanged = True
-            it = it + 1 if not alphaPairsChanged else 0
-        # alphas中大于零的样本点(xi, yi)的实例xi称为支持向量，或者软间隔支持向量
-        self.__b = b
+            # a1new = a1Old + yi* yj * (a2Old - a2new) / eta ==> update a1
+            alphas[i] += label_mat[j] * label_mat[i] * (alpha_j_old - alphas[j])
+
+            # 缓存一次Ei
+            self.__updateEk(i, alphas, label_mat, data_matrix, self.__b)
+
+            # 通过a更新b
+            b1 = self.__b - Ei - float(
+                label_mat[i] * (alphas[i] - alpha_i_old) * data_matrix[i, :] * data_matrix[i, :].T) - \
+                 float(label_mat[j] * (alphas[j] - alpha_j_old) * data_matrix[i, :] * data_matrix[j, :].T)
+            b2 = self.__b - Ej - float(
+                label_mat[i] * (alphas[i] - alpha_i_old) * data_matrix[i, :] * data_matrix[j, :].T) - \
+                 float(label_mat[j] * (alphas[j] - alpha_j_old) * data_matrix[j, :] * data_matrix[j, :].T)
+
+            if 0 < alphas[i] < C:
+                self.__b = b1
+            elif 0 < alphas[j] < C:
+                self.__b = b2
+            else:
+                self.__b = (b1 + b2) / 2.0
+        return True
