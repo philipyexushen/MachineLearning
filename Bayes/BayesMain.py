ea6d6f9737997fab0b5063b11d66cb31a6affb53
philipyexushen <923822778@qq.com>
2019-02-11 04:36:08 +0800

    byes implement

--- Bayes/BayesMain.py
+++ Bayes/BayesMain.py
@@ -1,9 +1,12 @@
 from Bayes.DocumentClassifier import *
 
 def main():
-    list_word, word_classified_map = load_data_set()
-    result_vocab_list = create_vocabList(list_word)
-    print(set_words_to_vec(result_vocab_list, list_word[0]))
+    classifier = DocumentClassifier()
+    classifier.load_data_set()
+    classifier.train()
+
+    ret = classifier.test(["stupid", "worthless", "dog"])
+    print(f"result={ret}")
 
 if __name__ == "__main__":
     main()
--- Bayes/DocumentClassifier.py
+++ Bayes/DocumentClassifier.py
@@ -1,28 +1,69 @@
 import numpy as np
 
-def load_data_set():
-    posting_list = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
-                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
-                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
-                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
-                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
-                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-    class_vec = [0, 1, 0, 1, 0, 1]  # 1表示侮辱性词，0表示非侮辱性词
-    return posting_list, class_vec
+class DocumentClassifier:
+    def __init__(self):
+        self.__posting_list = None
+        self.__class_vec = None
+        self.__vocab_set = None
+        self.__p0V = None
+        self.__p1V = None
+        self.__pAb = None
 
+    def load_data_set(self)->None:
+        posting_list = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                        ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                        ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                        ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                        ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                        ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+        class_vec = [0, 1, 0, 1, 0, 1]  # 1表示侮辱性词，0表示非侮辱性词
+        self.__posting_list = posting_list
+        self.__class_vec = class_vec
 
-def create_vocabList(dataSet):
-    vocab_set = set([])
-    for document in dataSet:
-        vocab_set = vocab_set | set(document)
-    return list(vocab_set)
+        vocab_set = set([])
+        for document in self.__posting_list:
+            vocab_set = vocab_set | set(document)
+        self.__vocab_set = list(vocab_set)
 
+    def train(self)->None:
+        train_mat = []
+        for doc in self.__posting_list:
+            train_mat.append(self.__set_words_to_vec(self.__vocab_set, doc))
+        self.__p0V, self.__p1V, self.__pAb = self.__trainNB0(np.array(train_mat), np.array(self.__class_vec))
 
-def set_words_to_vec(vocab_list, input_set):
-    return_vec = [0] * len(vocab_list)
-    for word in input_set:
-        if word in vocab_list:
-            return_vec[vocab_list.index(word)] = 1
-        else:
-            print("the word: %s is not in my Vocabulary!" % word)
-    return return_vec
+    def test(self, entry)->bool:
+        this_doc = np.array(self.__set_words_to_vec(self.__vocab_set, entry))
+        return self.__classifyNB(this_doc,  self.__p0V, self.__p1V, self.__pAb)
+
+    @staticmethod
+    def __trainNB0(train_matrix, train_category):
+        num_train_docs, num_words = np.shape(train_matrix)
+        p_abusive = sum(train_category) / float(num_train_docs)
+        p0_num = np.ones(num_words); p1_num = np.ones(num_words)  # change to ones()
+        p0_denominator = 2.0; p1_denominator = 2.0  # change to 2.0
+        for i in range(num_train_docs):
+            if train_category[i] == 1:
+                p1_num += train_matrix[i]
+                p1_denominator += sum(train_matrix[i])
+            else:
+                p0_num += train_matrix[i]
+                p0_denominator += sum(train_matrix[i])
+        p1_vec = np.log(p1_num / p1_denominator)
+        p0_vec = np.log(p0_num / p0_denominator)
+        # 得到各个词条件概率，以及先验概率pAbusive
+        return p0_vec, p1_vec, p_abusive
+
+    @staticmethod
+    def __classifyNB(vec2Classify, p0Vec, p1Vec, pClass1)->bool:
+        p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)  # element-wise mult
+        p0 = sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+        return p1 > p0
+
+    @staticmethod
+    def __set_words_to_vec(vocab_list, input_set):
+        return_vec = [0] * len(vocab_list)
+        for word in input_set:
+            if word in vocab_list:
+                return_vec[vocab_list.index(word)] = 1
+        return return_vec
+
