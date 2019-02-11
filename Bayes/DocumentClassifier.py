ddfc5f317441a4e00acd068c9e03de155039f2a8
philipyexushen <923822778@qq.com>
2019-02-11 17:32:08 +0800

    modify API

--- Bayes/DocumentClassifier.py
+++ Bayes/DocumentClassifier.py
@@ -29,27 +29,30 @@ class DocumentClassifier:
         return self.__classifyNB(this_doc,  self.__p0V, self.__p1V, self.__pAb)
 
     @staticmethod
-    def __trainNB0(train_matrix, train_category):
+    def __trainNB0(train_matrix:np.ndarray, train_category:np.ndarray):
         num_train_docs, num_words = np.shape(train_matrix)
-        p_abusive = sum(train_category) / float(num_train_docs)
+        p_prior = np.sum(train_category) / float(num_train_docs)
         p0_num = np.ones(num_words); p1_num = np.ones(num_words)  # change to ones()
-        p0_denominator = 2.0; p1_denominator = 2.0  # change to 2.0
+
+        # 拉普拉斯平滑，词只有出现和不出现两种情况
+        p0_denominator = 2.0; p1_denominator = 2.0
+
         for i in range(num_train_docs):
             if train_category[i] == 1:
                 p1_num += train_matrix[i]
-                p1_denominator += sum(train_matrix[i])
+                p1_denominator += np.sum(train_matrix[i])
             else:
                 p0_num += train_matrix[i]
-                p0_denominator += sum(train_matrix[i])
+                p0_denominator += np.sum(train_matrix[i])
         p1_vec = np.log(p1_num / p1_denominator)
         p0_vec = np.log(p0_num / p0_denominator)
-        # 得到各个词条件概率，以及先验概率pAbusive
-        return p0_vec, p1_vec, p_abusive
+        # 得到各个词条件概率，以及先验概率
+        return p0_vec, p1_vec, p_prior
 
     @staticmethod
     def __classifyNB(vec2Classify, p0Vec, p1Vec, pClass1)->bool:
-        p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)  # element-wise mult
-        p0 = sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+        p1 = np.sum(vec2Classify * p1Vec) + np.log(pClass1)  # element-wise mult
+        p0 = np.sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
         return p1 > p0
 
     @staticmethod
